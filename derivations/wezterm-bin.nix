80d00d3d85802963532426af334f8082e1d8af35
Ignacio Marmolejo <ignacio.marmolejo@oracle.com>
2024-02-25 12:14:15 -0800

    Update wezterm-bin derivation

--- derivations/wezterm-bin.nix
+++ derivations/wezterm-bin.nix
@@ -4,22 +4,23 @@
   ...
 }:
 pkgs.stdenv.mkDerivation rec {
-  pname = "Wezterm";
+  pname = "WezTerm";
   version = "20240203-110809-5046fc22";
 
-  phases = ["installPhase"];
+  phases = ["installPhase" "postInstall"];
   installPhase = ''
     mkdir -p $out/Applications
     echo $src
-    cp -avi $src/Wezterm.app $out/Applications/
+    cp -avi $src/${pname}.app $out/Applications/
   '';
 
   postInstall = ''
-    ln -s $out/Applications/Wezterm.app/Contents/MacOS/{wezterm,wezterm-gui} $out/bin
+    mkdir -p $out/bin
+    ln -s $out/Applications/${pname}.app/Contents/MacOS/{wezterm,wezterm-gui} $out/bin/
   '';
 
   src = pkgs.fetchzip {
-    url = "https://github.com/wez/wezterm/releases/download/${version}/WezTerm-macos-${version}.zip";
+    url = "https://github.com/wez/wezterm/releases/download/${version}/${pname}-macos-${version}.zip";
     sha256 = "sha256-HKUC7T7VJ+3dDtbOoFc/kVUBUGstsAZn+IpD9oRIMXw=";
   };
 
