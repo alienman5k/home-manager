82cbceea9cf19edf8250337cabc5239424d6a338
Ignacio Marmolejo <ignacio.marmolejo@oracle.com>
2024-03-27 10:42:08 -0700

    Use bruno from NixPkgs instead of custom derivation

--- modules/macos.nix
+++ modules/macos.nix
@@ -1,12 +1,18 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "obsidian-1.5.11"
+  ];
+
   home.packages = with pkgs;
   [
     # GUI Apps
     # raycast # Spotligh alternative
     # Custom Derivations
-    (callPackage ../derivations/bruno-bin.nix { })
+    # (callPackage ../derivations/bruno-bin.nix { })
     # (callPackage ../derivations/librewolf-bin.nix { })
     # (callPackage ../derivations/firefoxdev-bin.nix { })
+    bruno
+    # obsidian
   ];
 }
